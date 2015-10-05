35824ef889df8c8d0cf0285b585f4404b438a6a3
sasha2567 <lisyanscky.sasha2567@yandex.ru>
2015-10-05 12:06:08 +0300

    добавил папку с курсачем по вебу

---
+++ web/kursach/application/controllers/Adminadd.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Adminadd extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->helper('form');
+		$this->load->helper('url');
+		$this->load->view('admin/addrecipe');
+	}
+
+	public function add()
+	{
+		if(isset($_POST))
+		{
+			$description = $_POST['recipe_description'];
+			$ingredients = $_POST['recipe_ingridients'];
+			$recipe = $_POST['recipe_recipe'];
+			$data = array(
+	            'description' => $description,
+	            'ingredients' => $ingredients,
+	            'recipe' => $recipe
+	        );
+
+	        $this->load->model('recipe');
+	        $recipes = $this->recipe->add($data);
+	        redirect('adminindex');
+		}
+	}
+}
