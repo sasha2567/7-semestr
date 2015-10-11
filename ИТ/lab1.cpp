5f53fe2aa1955b3f65524d1cb557d04ebfa1c985
sasha2567 <lisyanscky.sasha2567@yandex.ru>
2015-10-11 22:14:43 +0300

    первая лаба по ИТ

--- ИТ/lab1.cpp
+++ ИТ/lab1.cpp
@@ -1,10 +1,37 @@
+#include <stdio.h>
 #include <string.h>
+#include <conio.h>
+#include <iostream>
+#include <tchar.h>
+using namespace std;
 
-int main(){
-    string s = "Поменьше говори - больше услышишь"
-
-    int a[20];
-    for(int i=0; i<lenth(s); i++)
+int main()
+{
+    string s = "Поменьше говори - больше услышишь";
+    int length_leter = 0;
+    char leters[20]={0};
+    int freq[20]={0};
+    for(int i=0; i<strlen(s); i++)
+    {
+        int flag = 0;
+        for(int j=0; j<length_leter); j++)
+            if(s[i] == leters[j])
+            {
+                freq[j]++;
+                flag = 1;
+                break;
+            }
+        if(flag == 0)
+        {
+            leters[length_leter]=s[i];
+            freq[length_leter] = 1;
+            length_leter++;
+        }
+    }
+    for(int i=0; i<length_leter; i++)
+    {
+        printf("%c %d/n",leters[i],freq[i]);
+    }
 
     return 0;
 }
